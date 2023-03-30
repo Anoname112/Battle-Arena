60a61830855fd66ed35cac806d36221e68cbe1f4
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-03-30 08:40:30 +0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -2,18 +2,20 @@ const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 const isPortrait = window.innerWidth < window.innerHeight;
 
 const bodyMargin = "0";
-const bodyBackColor = "#fff";
-const bodyTextColor = "#000";
-const bodyFont = "15px Segoe UI";
+const bodyBackColor = "transparent";
+const bodyTextColor = "#000000";
+const bodyFontSize = 15;
+const bodyFont = bodyFontSize + "px Segoe UI";
 const canvasBorderRadius = 0;
-const canvasBackColor = "#333";
+const canvasBackColor = "#333333";
 const canvasPosition = "fixed";
 const canvasCursor = "pointer";
 const canvasCursorAttack = "crosshair";
 const audioVisibility = "hidden";
 const msgY = 15;
-const msgTextColor = "#000";
-const msgFont = "13px Consolas";
+const msgTextColor = "#000000";
+const msgFontSize = 13;
+const msgFont = msgFontSize + "px Consolas";
 
 const upVec2 = new Vec2(0, -1);
 const mapWidth = 1455;
@@ -21,8 +23,8 @@ const mapHeight = 726;
 const minionSpawnTime = 600;
 const hpBarDistance = 10;
 const hpBarColor = {
-	0: "#0f0",
-	1: "#f00"
+	0: "#00FF00",
+	1: "#FF0000"
 };
 
 // Image
