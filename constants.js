995d14aa033e50491ebf8f8ce9097378a3da6772
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-04-05 21:29:31 +0700

    Update constants

--- constants.js
+++ constants.js
@@ -1,27 +1,6 @@
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 const isPortrait = window.innerWidth < window.innerHeight;
 
-const bodyMargin = "0";
-const bodyBackColor = "transparent";
-const bodyFontColor = "#000000";
-const bodyFontSize = 15;
-const bodyFont = bodyFontSize + "px Segoe UI";
-const canvasBorderRadius = 0;
-const canvasBackColor = "#444444";
-const canvasPosition = "fixed";
-const canvasCursor = "default";
-const canvasCursorAttack = "crosshair";
-const controlPadding = 10;
-const controlSize = 55;
-const audioVisibility = "hidden";
-const msgY = 20;
-const msgFontColor = "#FFFFFF";
-const msgFontSize = 13;
-const msgFont = msgFontSize + "px Consolas";
-const msgBackPadding = 5;
-const msgBackRadius = 5;
-const msgBackColor = "#222222";
-
 const interval = 15;
 const upVec2 = new Vec2(0, -1);
 const mapWidth = 1455;
@@ -36,6 +15,36 @@ const hpBarColor = {
 const cameraSpeed = 20;
 const heartAnimSpeed = 0.3;
 const heartAnimMax = 3;
+
+// Body
+const bodyMargin = "0";
+const bodyBackColor = "transparent";
+const bodyFontColor = "#000000";
+const bodyFontSize = 15;
+const bodyFont = bodyFontSize + "px Segoe UI";
+
+// Canvas
+const canvasBorderRadius = 0;
+const canvasBackColor = "#444444";
+const canvasPosition = "fixed";
+const canvasCursor = "default";
+const canvasCursorAttack = "crosshair";
+
+// Message
+const msgY = 20;
+const msgFontColor = "#FFFFFF";
+const msgFontSize = 13;
+const msgFont = msgFontSize + "px Consolas";
+const msgBackPadding = 5;
+const msgBackRadius = 5;
+const msgBackColor = "#222222";
+
+// Control
+const controlPadding = 10;
+const controlSize = 55;
+
+// Sound
+const audioVisibility = "hidden";
 
 // Image
 const images = [];
